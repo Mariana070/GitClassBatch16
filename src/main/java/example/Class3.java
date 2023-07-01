334538a39cc5ba274f334b5fb01592e7469d8f74
Mariana <kyzmyn.mariana@gmail.com>
2023-07-01 14:33:49 -0500

    selenium new

---
+++ src/main/java/example/Class3.java
@@ -0,0 +1,7 @@
+package example;
+
+public class Class3 {
+    public static void main(String[] args) {
+//We are trying to see the PR - pull request
+    }
+}
---
+++ src/main/java/example/Class4.java
@@ -0,0 +1,7 @@
+package example;
+
+public class Class4 {
+    public static void main(String[] args) {
+
+    }
+}
